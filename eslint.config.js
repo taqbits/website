c412e400eb92a4c8dda2b2e9c8a9145e0f61ac92
christophersesugh <christohybrid185@gmail.com>
2025-02-09 13:54:30 +0100

    style: configure eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,14 +2,53 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import hooksPlugin from "eslint-plugin-react-hooks";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  { ignores: ["node_modules", "dist", "build", "coverage", "public", "eslint.config.js"] },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
   eslintPluginPrettierRecommended,
+  {
+    rules: {
+      "no-empty": "warn",
+      "no-empty-pattern": "off", // Disable this rule for RR meta empty object destructuring
+      "@typescript-eslint/no-namespace": "off", // Disable this rule for RR routes types
+    },
+  },
+  {
+    plugins: {
+      react: pluginReact,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      ...pluginReact.configs.recommended.rules,
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+  {
+    plugins: {
+      "react-hooks": hooksPlugin,
+    },
+    rules: {
+      ...hooksPlugin.configs.recommended.rules,
+    },
+  },
+  {
+    plugins: {
+      tseslint: tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      "@typescript-eslint/no-namespaces": "off",
+    },
+  },
 ];
